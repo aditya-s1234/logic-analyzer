3705dc610ce368709fba72b26245ed60964cb01d
aditya-s1234 <adityasubramanian16@gmail.comp>
2026-08-25 20:34:17 -0700

    Timer config and basic sampling with interrupt - initialized timer to start at 0, count to 159 before sending signal for interrupt to execute - 16 MHz signal, prescalar of 1, and TOP = 159, which means a 10 kHz sampling frequency

--- src/main.cpp
+++ src/main.cpp
@@ -1,4 +1,7 @@
 #include <Arduino.h>
+
+volatile uint8_t snapshot{};
+
 void setup() {
 // write your initialization code here
 
@@ -10,8 +13,6 @@ void setup() {
     DDRB &= ~(1 << PB3);
     DDRB &= ~(1 << PB4);
     DDRB &= ~(1 << PB5);
-
-
     //since it's input this is similar to digitalwrite so they will have pullup turned on
     PORTB |= (1 <<PB0);
     PORTB |= (1 <<PB1);
@@ -19,11 +20,36 @@ void setup() {
     PORTB |= (1 <<PB3);
     PORTB |= (1 <<PB4);
     PORTB |= (1 <<PB5);
+    //i should only have wgm12 turned on which is 3rd bit in register TCCR1b so im setting iit here
+    //this combo gives mode 4 which will do CTC with OCR1A as max
+    TCCR1A &= ~(1 << 0);
+    TCCR1A &= ~(1 << 1);
+
+    TCCR1B |= (1 << 3);
+    TCCR1B &= ~(1 << 4);
+    //first three bits are CS1 bits, determines what prescaler should be based on these bits. i want prescaler
+    // of 1 so it should be 0 0 1
+    TCCR1B |= (1 << 0);
+    TCCR1B &= ~(1 << 1);
+    TCCR1B &= ~(1 << 2);
+    //setting counter to 0
+    TCNT1 = 0;
+    //doing 100 khz sampling so for 16 mhz arduino clock i will do 159 samples as max (0 counts)
+    OCR1A = 159;
+    //timsk1 handles interrupts, bit 1 is OCIE1A, makes timer interrupt when ocr1a is matched
+    TIMSK1 |= (1 << 1);
 }
+
+
+ISR(TIMER1_COMPA_vect) {
+    //samples here
+    snapshot = PINB;
+}
+
 
 void loop() {
 // write your code here
-    Serial.println(PINB, BIN);
-    delay(1000);
+    Serial.println(snapshot, BIN);
+    delay(500);
 
 }
