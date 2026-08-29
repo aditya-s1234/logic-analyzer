cabfe99d573a815b685a922f6342da8cff33bb08
aditya-s1234 <adityasubramanian16@gmail.comp>
2026-08-28 22:50:45 -0700

    Created transition detector and duration detector - through detectTransitions and timing classes, vectors with data on specific channel signals and duration are accurately recorded

--- desktop/src/Capture.cpp
+++ desktop/src/Capture.cpp
@@ -2,6 +2,9 @@
 #include <iostream>
 using namespace std::chrono_literals;
 
+
+//using chrono to time program to keep doing reads of the 512 byte buffer until
+//the time limit is reached
 void Capture::fillBuffer() {
     auto start{std::chrono::steady_clock::now()};
     int reads{};
@@ -10,8 +13,35 @@ void Capture::fillBuffer() {
         m_allSamples.insert(m_allSamples.end(), std::begin(m_port.getBuffer()), std::end(m_port.getBuffer()));
         ++reads;
     }
-    auto elapsed = std::chrono::steady_clock::now() - start;
+}
 
+//should append the sample numberwhen it changes
+void Capture::detectTransitions() {
+    m_allTransitions.clear();
+    for (std::ptrdiff_t i{}; i < std::ssize(m_allSamples)-1; ++i) {
+        if (m_allSamples[i] != m_allSamples[i+1]) {
+            Transition x;
+            x.sample = i+1;
+            //here the xor identifies when two samples are diff
+            x.bitsChanged = m_allSamples[i] ^ m_allSamples[i+1];
+            m_allTransitions.push_back(x);
+        }
+    }
+}
 
-
+//
+void Capture::timing() {
+    m_allTimings.clear();
+    //bits should start high
+    int startPos{};
+    for (std::ptrdiff_t i{}; i < std::ssize(m_allTransitions); ++i) {
+        Levels x;
+        //this should compare two specific bits on both uint8s, with &1 clearing everythng else
+        if ((m_allTransitions[i].bitsChanged >> m_channel) & 1) {
+            x.volts = (m_allSamples[m_allTransitions[i].sample-1] >> m_channel) & 1;
+            x.duration = m_allTransitions[i].sample - startPos;
+            startPos = m_allTransitions[i].sample;
+            m_allTimings.push_back(x);
+        }
+    }
 }
--- desktop/src/Capture.h
+++ desktop/src/Capture.h
@@ -3,20 +3,51 @@
 #include <vector>
 #include <chrono>
 
+//this is for samples that are different from their previous one
+struct Transition {
+    int sample{};
+    uint8_t bitsChanged{};
+};
+
+//
+struct Levels {
+    bool volts{};
+    int duration{};
+};
+
 
 class Capture {
 private:
     std::vector<uint8_t> m_allSamples{};
     std::chrono::milliseconds m_target{};
     SerialPort m_port;
+    std::vector<Transition> m_allTransitions{};
+    std::vector<Levels> m_allTimings{};
+    int m_channel{5};
+
 public:
     Capture (SerialPort& port, int target)
-        :m_target{target}, m_port{port}
-    {}
+        :m_target{target}, m_port{port} {
+        m_allSamples.reserve(target/1000 * 100000);
+    }
 
     std::vector<uint8_t>& getSamples() {
         return m_allSamples;
     }
 
+    std::vector<Transition>& getTransitions() {
+        return m_allTransitions;
+    }
+
+    std::vector<Levels>& getTimings() {
+        return m_allTimings;
+    }
+
+    void setChannel(int x) {
+        m_channel = x;
+    }
+
     void fillBuffer();
+    void detectTransitions();
+    void timing();
 };
